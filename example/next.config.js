49acfd44cd762b9a222d7c9c77cab7d6f98117e3
Gavyn McKenzie <gavyn@etchapps.com>
2020-05-14 16:52:52 +0100

    fix: Basepath support for gh pages

--- example/next.config.js
+++ example/next.config.js
@@ -3,5 +3,8 @@ const withSourceMaps = require('@zeit/next-source-maps')();
 const isProd = (process.env.NODE_ENV || 'production') === 'production'
 
 module.exports = withSourceMaps({
-  assetPrefix: isProd ? '/next-pagination' : ''
+  assetPrefix: isProd ? '/next-pagination' : '',
+  experimental: {
+    basePath: isProd ? '/next-pagination' : ''
+  }
 });
