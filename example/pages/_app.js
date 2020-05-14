98d25678ae7d61972d8162f3d0c81782633f9ebc
Gavyn McKenzie <gavyn@etchapps.com>
2020-05-14 16:54:06 +0100

    chore: Page title

--- example/pages/_app.js
+++ example/pages/_app.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Head from 'next/head';
 
 import 'next-pagination/dist/index.css'
 import '../styles/main.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <title>Next pagination</title>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
