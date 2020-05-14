5226c4f127d0d1ee475f381d50a00688d036cc47
Gavyn McKenzie <gavyn@etchapps.com>
2020-05-14 15:20:44 +0100

    Updates

---
+++ _next/static/webpack/static/development/pages/index.js.2a3d509c5f1f914b30da.hot-update.js
@@ -0,0 +1,575 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "../dist/index.modern.js":
+/*!*******************************!*\
+  !*** ../dist/index.modern.js ***!
+  \*******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuLi9kaXN0L2luZGV4Lm1vZGVybi5qcy5qcyIsInNvdXJjZXNDb250ZW50IjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///../dist/index.modern.js\n");
+
+/***/ }),
+
+/***/ "../node_modules/classnames/index.js":
+false,
+
+/***/ "../node_modules/decode-uri-component/index.js":
+false,
+
+/***/ "../node_modules/lodash/_DataView.js":
+false,
+
+/***/ "../node_modules/lodash/_Hash.js":
+false,
+
+/***/ "../node_modules/lodash/_ListCache.js":
+false,
+
+/***/ "../node_modules/lodash/_Map.js":
+false,
+
+/***/ "../node_modules/lodash/_MapCache.js":
+false,
+
+/***/ "../node_modules/lodash/_Promise.js":
+false,
+
+/***/ "../node_modules/lodash/_Set.js":
+false,
+
+/***/ "../node_modules/lodash/_SetCache.js":
+false,
+
+/***/ "../node_modules/lodash/_Stack.js":
+false,
+
+/***/ "../node_modules/lodash/_Symbol.js":
+false,
+
+/***/ "../node_modules/lodash/_Uint8Array.js":
+false,
+
+/***/ "../node_modules/lodash/_WeakMap.js":
+false,
+
+/***/ "../node_modules/lodash/_arrayFilter.js":
+false,
+
+/***/ "../node_modules/lodash/_arrayLikeKeys.js":
+false,
+
+/***/ "../node_modules/lodash/_arrayMap.js":
+false,
+
+/***/ "../node_modules/lodash/_arrayPush.js":
+false,
+
+/***/ "../node_modules/lodash/_arraySome.js":
+false,
+
+/***/ "../node_modules/lodash/_assignValue.js":
+false,
+
+/***/ "../node_modules/lodash/_assocIndexOf.js":
+false,
+
+/***/ "../node_modules/lodash/_baseAssignValue.js":
+false,
+
+/***/ "../node_modules/lodash/_baseGet.js":
+false,
+
+/***/ "../node_modules/lodash/_baseGetAllKeys.js":
+false,
+
+/***/ "../node_modules/lodash/_baseGetTag.js":
+false,
+
+/***/ "../node_modules/lodash/_baseHasIn.js":
+false,
+
+/***/ "../node_modules/lodash/_baseIsArguments.js":
+false,
+
+/***/ "../node_modules/lodash/_baseIsEqual.js":
+false,
+
+/***/ "../node_modules/lodash/_baseIsEqualDeep.js":
+false,
+
+/***/ "../node_modules/lodash/_baseIsMatch.js":
+false,
+
+/***/ "../node_modules/lodash/_baseIsNative.js":
+false,
+
+/***/ "../node_modules/lodash/_baseIsTypedArray.js":
+false,
+
+/***/ "../node_modules/lodash/_baseIteratee.js":
+false,
+
+/***/ "../node_modules/lodash/_baseKeys.js":
+false,
+
+/***/ "../node_modules/lodash/_baseKeysIn.js":
+false,
+
+/***/ "../node_modules/lodash/_baseMatches.js":
+false,
+
+/***/ "../node_modules/lodash/_baseMatchesProperty.js":
+false,
+
+/***/ "../node_modules/lodash/_basePickBy.js":
+false,
+
+/***/ "../node_modules/lodash/_baseProperty.js":
+false,
+
+/***/ "../node_modules/lodash/_basePropertyDeep.js":
+false,
+
+/***/ "../node_modules/lodash/_baseSet.js":
+false,
+
+/***/ "../node_modules/lodash/_baseTimes.js":
+false,
+
+/***/ "../node_modules/lodash/_baseToString.js":
+false,
+
+/***/ "../node_modules/lodash/_baseUnary.js":
+false,
+
+/***/ "../node_modules/lodash/_cacheHas.js":
+false,
+
+/***/ "../node_modules/lodash/_castPath.js":
+false,
+
+/***/ "../node_modules/lodash/_coreJsData.js":
+false,
+
+/***/ "../node_modules/lodash/_defineProperty.js":
+false,
+
+/***/ "../node_modules/lodash/_equalArrays.js":
+false,
+
+/***/ "../node_modules/lodash/_equalByTag.js":
+false,
+
+/***/ "../node_modules/lodash/_equalObjects.js":
+false,
+
+/***/ "../node_modules/lodash/_freeGlobal.js":
+false,
+
+/***/ "../node_modules/lodash/_getAllKeys.js":
+false,
+
+/***/ "../node_modules/lodash/_getAllKeysIn.js":
+false,
+
+/***/ "../node_modules/lodash/_getMapData.js":
+false,
+
+/***/ "../node_modules/lodash/_getMatchData.js":
+false,
+
+/***/ "../node_modules/lodash/_getNative.js":
+false,
+
+/***/ "../node_modules/lodash/_getPrototype.js":
+false,
+
+/***/ "../node_modules/lodash/_getRawTag.js":
+false,
+
+/***/ "../node_modules/lodash/_getSymbols.js":
+false,
+
+/***/ "../node_modules/lodash/_getSymbolsIn.js":
+false,
+
+/***/ "../node_modules/lodash/_getTag.js":
+false,
+
+/***/ "../node_modules/lodash/_getValue.js":
+false,
+
+/***/ "../node_modules/lodash/_hasPath.js":
+false,
+
+/***/ "../node_modules/lodash/_hashClear.js":
+false,
+
+/***/ "../node_modules/lodash/_hashDelete.js":
+false,
+
+/***/ "../node_modules/lodash/_hashGet.js":
+false,
+
+/***/ "../node_modules/lodash/_hashHas.js":
+false,
+
+/***/ "../node_modules/lodash/_hashSet.js":
+false,
+
+/***/ "../node_modules/lodash/_isIndex.js":
+false,
+
+/***/ "../node_modules/lodash/_isKey.js":
+false,
+
+/***/ "../node_modules/lodash/_isKeyable.js":
+false,
+
+/***/ "../node_modules/lodash/_isMasked.js":
+false,
+
+/***/ "../node_modules/lodash/_isPrototype.js":
+false,
+
+/***/ "../node_modules/lodash/_isStrictComparable.js":
+false,
+
+/***/ "../node_modules/lodash/_listCacheClear.js":
+false,
+
+/***/ "../node_modules/lodash/_listCacheDelete.js":
+false,
+
+/***/ "../node_modules/lodash/_listCacheGet.js":
+false,
+
+/***/ "../node_modules/lodash/_listCacheHas.js":
+false,
+
+/***/ "../node_modules/lodash/_listCacheSet.js":
+false,
+
+/***/ "../node_modules/lodash/_mapCacheClear.js":
+false,
+
+/***/ "../node_modules/lodash/_mapCacheDelete.js":
+false,
+
+/***/ "../node_modules/lodash/_mapCacheGet.js":
+false,
+
+/***/ "../node_modules/lodash/_mapCacheHas.js":
+false,
+
+/***/ "../node_modules/lodash/_mapCacheSet.js":
+false,
+
+/***/ "../node_modules/lodash/_mapToArray.js":
+false,
+
+/***/ "../node_modules/lodash/_matchesStrictComparable.js":
+false,
+
+/***/ "../node_modules/lodash/_memoizeCapped.js":
+false,
+
+/***/ "../node_modules/lodash/_nativeCreate.js":
+false,
+
+/***/ "../node_modules/lodash/_nativeKeys.js":
+false,
+
+/***/ "../node_modules/lodash/_nativeKeysIn.js":
+false,
+
+/***/ "../node_modules/lodash/_nodeUtil.js":
+false,
+
+/***/ "../node_modules/lodash/_objectToString.js":
+false,
+
+/***/ "../node_modules/lodash/_overArg.js":
+false,
+
+/***/ "../node_modules/lodash/_root.js":
+false,
+
+/***/ "../node_modules/lodash/_setCacheAdd.js":
+false,
+
+/***/ "../node_modules/lodash/_setCacheHas.js":
+false,
+
+/***/ "../node_modules/lodash/_setToArray.js":
+false,
+
+/***/ "../node_modules/lodash/_stackClear.js":
+false,
+
+/***/ "../node_modules/lodash/_stackDelete.js":
+false,
+
+/***/ "../node_modules/lodash/_stackGet.js":
+false,
+
+/***/ "../node_modules/lodash/_stackHas.js":
+false,
+
+/***/ "../node_modules/lodash/_stackSet.js":
+false,
+
+/***/ "../node_modules/lodash/_stringToPath.js":
+false,
+
+/***/ "../node_modules/lodash/_toKey.js":
+false,
+
+/***/ "../node_modules/lodash/_toSource.js":
+false,
+
+/***/ "../node_modules/lodash/eq.js":
+false,
+
+/***/ "../node_modules/lodash/get.js":
+false,
+
+/***/ "../node_modules/lodash/hasIn.js":
+false,
+
+/***/ "../node_modules/lodash/identity.js":
+false,
+
+/***/ "../node_modules/lodash/isArguments.js":
+false,
+
+/***/ "../node_modules/lodash/isArray.js":
+false,
+
+/***/ "../node_modules/lodash/isArrayLike.js":
+false,
+
+/***/ "../node_modules/lodash/isBuffer.js":
+false,
+
+/***/ "../node_modules/lodash/isEmpty.js":
+false,
+
+/***/ "../node_modules/lodash/isFunction.js":
+false,
+
+/***/ "../node_modules/lodash/isLength.js":
+false,
+
+/***/ "../node_modules/lodash/isObject.js":
+false,
+
+/***/ "../node_modules/lodash/isObjectLike.js":
+false,
+
+/***/ "../node_modules/lodash/isSymbol.js":
+false,
+
+/***/ "../node_modules/lodash/isTypedArray.js":
+false,
+
+/***/ "../node_modules/lodash/keys.js":
+false,
+
+/***/ "../node_modules/lodash/keysIn.js":
+false,
+
+/***/ "../node_modules/lodash/memoize.js":
+false,
+
+/***/ "../node_modules/lodash/pickBy.js":
+false,
+
+/***/ "../node_modules/lodash/property.js":
+false,
+
+/***/ "../node_modules/lodash/stubArray.js":
+false,
+
+/***/ "../node_modules/lodash/stubFalse.js":
+false,
+
+/***/ "../node_modules/lodash/toString.js":
+false,
+
+/***/ "../node_modules/query-string/index.js":
+false,
+
+/***/ "../node_modules/split-on-first/index.js":
+false,
+
+/***/ "../node_modules/strict-uri-encode/index.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/arrayLikeToArray.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/arrayWithHoles.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/assertThisInitialized.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/asyncToGenerator.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/classCallCheck.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/construct.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/createClass.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/getPrototypeOf.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/inherits.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/interopRequireDefault.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/interopRequireWildcard.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/isNativeReflectConstruct.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/iterableToArrayLimit.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/nonIterableRest.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/possibleConstructorReturn.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/setPrototypeOf.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/slicedToArray.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/typeof.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/unsupportedIterableToArray.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/regenerator/index.js":
+false,
+
+/***/ "./node_modules/function-bind/implementation.js":
+false,
+
+/***/ "./node_modules/function-bind/index.js":
+false,
+
+/***/ "./node_modules/has/src/index.js":
+false,
+
+/***/ "./node_modules/native-url/dist/index.js":
+false,
+
+/***/ "./node_modules/next/dist/build/polyfills/object-assign.js":
+false,
+
+/***/ "./node_modules/next/dist/build/polyfills/object.assign/index.js":
+false,
+
+/***/ "./node_modules/next/dist/client/link.js":
+false,
+
+/***/ "./node_modules/next/dist/client/router.js":
+false,
+
+/***/ "./node_modules/next/dist/client/with-router.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/mitt.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router-context.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/router.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/is-dynamic.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/route-matcher.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/route-regex.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/utils.js":
+false,
+
+/***/ "./node_modules/next/link.js":
+false,
+
+/***/ "./node_modules/next/node_modules/prop-types/checkPropTypes.js":
+false,
+
+/***/ "./node_modules/next/node_modules/prop-types/factoryWithTypeCheckers.js":
+false,
+
+/***/ "./node_modules/next/node_modules/prop-types/index.js":
+false,
+
+/***/ "./node_modules/next/node_modules/prop-types/lib/ReactPropTypesSecret.js":
+false,
+
+/***/ "./node_modules/next/node_modules/webpack/buildin/global.js":
+false,
+
+/***/ "./node_modules/next/node_modules/webpack/buildin/module.js":
+false,
+
+/***/ "./node_modules/prop-types-exact/build/helpers/isPlainObject.js":
+false,
+
+/***/ "./node_modules/prop-types-exact/build/index.js":
+false,
+
+/***/ "./node_modules/querystring-es3/decode.js":
+false,
+
+/***/ "./node_modules/querystring-es3/encode.js":
+false,
+
+/***/ "./node_modules/querystring-es3/index.js":
+false,
+
+/***/ "./node_modules/react-is/cjs/react-is.development.js":
+false,
+
+/***/ "./node_modules/react-is/index.js":
+false,
+
+/***/ "./node_modules/regenerator-runtime/runtime.js":
+false,
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* WEBPACK VAR INJECTION */(function(module) {/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"default\", function() { return Home; });\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ \"../node_modules/react/index.js\");\n/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var next_pagination__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next-pagination */ \"../dist/index.modern.js\");\n/* harmony import */ var next_pagination__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_pagination__WEBPACK_IMPORTED_MODULE_1__);\nvar _jsxFileName = \"/Users/gavynmckenzie/dev/best-damn-pagination/example/pages/index.js\";\nvar __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;\n\n\nfunction Home() {\n  return __jsx(\"div\", {\n    __self: this,\n    __source: {\n      fileName: _jsxFileName,\n      lineNumber: 7,\n      columnNumber: 5\n    }\n  }, __jsx(\"h1\", {\n    __self: this,\n    __source: {\n      fileName: _jsxFileName,\n      lineNumber: 8,\n      columnNumber: 7\n    }\n  }, \"Pagination\"), __jsx(next_pagination__WEBPACK_IMPORTED_MODULE_1__[\"Pagination\"], {\n    total: 1000,\n    __self: this,\n    __source: {\n      fileName: _jsxFileName,\n      lineNumber: 9,\n      columnNumber: 7\n    }\n  }));\n}\n_c = Home;\n\nvar _c;\n\n$RefreshReg$(_c, \"Home\");\n\n;\n    var _a, _b;\n    // Legacy CSS implementations will `eval` browser code in a Node.js context\n    // to extract CSS. For backwards compatibility, we need to check we're in a\n    // browser context before continuing.\n    if (typeof self !== 'undefined' &&\n        // AMP / No-JS mode does not inject these helpers:\n        '$RefreshHelpers$' in self) {\n        var currentExports_1 = module.__proto__.exports;\n        var prevExports = (_b = (_a = module.hot.data) === null || _a === void 0 ? void 0 : _a.prevExports) !== null && _b !== void 0 ? _b : null;\n        // This cannot happen in MainTemplate because the exports mismatch between\n        // templating and execution.\n        self.$RefreshHelpers$.registerExportsForReactRefresh(currentExports_1, module.i);\n        // A module can be accepted automatically based on its exports, e.g. when\n        // it is a Refresh Boundary.\n        if (self.$RefreshHelpers$.isReactRefreshBoundary(currentExports_1)) {\n            // Save the previous exports on update so we can compare the boundary\n            // signatures.\n            module.hot.dispose(function (data) {\n                data.prevExports = currentExports_1;\n            });\n            // Unconditionally accept an update to this module, we'll check if it's\n            // still a Refresh Boundary later.\n            module.hot.accept();\n            // This field is set when the previous version of this module was a\n            // Refresh Boundary, letting us know we need to check for invalidation or\n            // enqueue an update.\n            if (prevExports !== null) {\n                // A boundary can become ineligible if its exports are incompatible\n                // with the previous exports.\n                //\n                // For example, if you add/remove/change exports, we'll want to\n                // re-execute the importing modules, and force those components to\n                // re-render. Similarly, if you convert a class component to a\n                // function, we want to invalidate the boundary.\n                if (self.$RefreshHelpers$.shouldInvalidateReactRefreshBoundary(prevExports, currentExports_1)) {\n                    module.hot.invalidate();\n                }\n                else {\n                    self.$RefreshHelpers$.scheduleUpdate();\n                }\n            }\n        }\n        else {\n            // Since we just executed the code for the module, it's possible that the\n            // new exports made it ineligible for being a boundary.\n            // We only care about the case when we were _previously_ a boundary,\n            // because we already accepted this update (accidental side effect).\n            var isNoLongerABoundary = prevExports !== null;\n            if (isNoLongerABoundary) {\n                module.hot.invalidate();\n            }\n        }\n    }\n\n/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../node_modules/next/node_modules/webpack/buildin/harmony-module.js */ \"./node_modules/next/node_modules/webpack/buildin/harmony-module.js\")(module)))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9wYWdlcy9pbmRleC5qcz80NGQ4Il0sIm5hbWVzIjpbIkhvbWUiXSwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQUE7QUFFQTtBQUVlLFNBQVNBLElBQVQsR0FBZ0I7QUFDN0IsU0FDRTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBLEtBQ0U7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQSxrQkFERixFQUVFLE1BQUMsMERBQUQ7QUFBWSxTQUFLLEVBQUUsSUFBbkI7QUFBQTtBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQSxJQUZGLENBREY7QUFNRDtLQVB1QkEsSSIsImZpbGUiOiIuL3BhZ2VzL2luZGV4LmpzLmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFJlYWN0IGZyb20gJ3JlYWN0JztcblxuaW1wb3J0IHsgUGFnaW5hdGlvbiB9IGZyb20gJ25leHQtcGFnaW5hdGlvbidcblxuZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gSG9tZSgpIHtcbiAgcmV0dXJuIChcbiAgICA8ZGl2PlxuICAgICAgPGgxPlBhZ2luYXRpb248L2gxPlxuICAgICAgPFBhZ2luYXRpb24gdG90YWw9ezEwMDB9IC8+XG4gICAgPC9kaXY+XG4gICk7XG59XG4iXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./pages/index.js\n");
+
+/***/ })
+
+})
