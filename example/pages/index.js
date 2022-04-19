c87bf74903a9acaa85f772e5f0e59755eb3cbe93
Gavyn McKenzie <gavyn@etch.co>
2022-04-19 17:07:05 +0100

    fix: example url

--- example/pages/index.js
+++ example/pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
 
       <h2>Other examples</h2>
       <p>
-        <Link href="/[dynamic]" as="/dynamic-test">
+        <Link href="[dynamic]" as="dynamic-test">
           <a>Dynamic pagination</a>
         </Link>
       </p>
