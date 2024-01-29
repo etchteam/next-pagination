4c542318181ab80792e83a9c86cc56f9da35d8a2
Josh Rose <josh@etch.co>
2024-01-29 19:27:06 +0100

    feat: bring into line with current lint setup

---
+++ lint-staged.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  '*.{ts,tsx,js,jsx,json,yaml,yml}': 'eslint --fix',
+  '*.{css,scss,tsx,jsx}': 'stylelint --fix',
+};
---
+++ stylelint.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  extends: ['@etchteam/stylelint-config'],
+};
