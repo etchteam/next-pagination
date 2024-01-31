7dfae169ac68e1c34b13b2240acbdacd1cba5514
Josh Rose <josh@etch.co>
2024-01-31 17:03:43 +0100

    feat: clean up example packages

--- example/next.config.js
+++ example/next.config.js
@@ -1,9 +1,6 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const withSourceMaps = require('@zeit/next-source-maps')();
-
 const isProd = (process.env.NODE_ENV || 'production') === 'production';
 
-module.exports = withSourceMaps({
+module.exports = {
   assetPrefix: isProd ? '/next-pagination' : undefined,
   basePath: isProd ? '/next-pagination' : undefined,
-});
+};
